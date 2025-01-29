f0c6158269440dcad9ce15dbf80dfca7d9c67b1c
Nuno Alves <nuno.alves@ctw.bmwgroup.com>
2025-01-29 10:11:46 +0000

    regexValidatePinCode / update arrayDiff file

--- arrayDiff.js
+++ arrayDiff.js
@@ -1,3 +1,6 @@
+// Codewars Exercise
+// https://www.codewars.com/kata/523f5d21c841566fde000009
+
 function arrayDiff(a, b) {
   let differencesArray = [];
   a.map((value) => (!b.includes(value) ? differencesArray.push(value) : null));
